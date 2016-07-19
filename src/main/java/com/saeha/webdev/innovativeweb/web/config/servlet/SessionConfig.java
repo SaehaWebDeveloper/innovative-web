0c17d4cb663012c2726684cd0e59f6001af10fe7
Lee Jang Young <leejy137@gmail.com>
2016-07-19 09:20:13 +0900

    spring session 소스 정리

--- src/main/java/com/saeha/webdev/innovativeweb/web/config/servlet/SessionConfig.java
+++ src/main/java/com/saeha/webdev/innovativeweb/web/config/servlet/SessionConfig.java
@@ -1,13 +1,7 @@
 package com.saeha.webdev.innovativeweb.web.config.servlet;
 
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
-
-import com.hazelcast.config.SerializerConfig;
-import com.hazelcast.core.HazelcastInstance;
 
 /**
  * Session Cluster (dependency: spring-session 1.2.0.RELEASE, hazelcast)
@@ -18,19 +12,4 @@ import com.hazelcast.core.HazelcastInstance;
 @Configuration
 @EnableHazelcastHttpSession(maxInactiveIntervalInSeconds=1800)
 public class SessionConfig {
-
-	/**
-	 * Hazelcast Instance
-	 */
-	@Autowired(required = false)
-	HazelcastInstance hazelcastInstance;
-
-	@PostConstruct
-	public void init() {
-		if (hazelcastInstance != null) {
-//			SerializerConfig serializerConfig = new SerializerConfig().setTypeClass(Object.class)
-//					.setImplementation(new ObjectStreamSerializer());
-//			hazelcastInstance.getConfig().getSerializationConfig().addSerializerConfig(serializerConfig);
-		}
-	}
 }
